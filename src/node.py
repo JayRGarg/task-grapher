00fafb73ae5152d15f5ee4ca3786c09a52d1f89e
JayRGarg <jayrgarg@gmail.com>
2025-04-23 19:24:15 -0700

    add date and time to node

--- src/node.py
+++ src/node.py
@@ -1,10 +1,20 @@
+from datetime import datetime, date, time
+
 class Node:
 
-    def __init__(self, value: str) -> None:
+    def __init__(self, value: str, due_date: date|None=None, due_time: time|None=None) -> None:
         self._value: str = value
+        self._created: datetime = datetime.now()
+        self._due_date: date|None = due_date
+        self._due_time: time|None = due_time
+        self._completed: bool = False
+
         self._children: list[Node] = []
         self._siblings: list[Node] = []
         self._parents: list[Node] = []
+        self._max_children: int = 4
+        self._max_parents: int = 4
+        #self._time
 
     def get_value(self) -> str:
         return self._value
@@ -13,4 +23,18 @@ class Node:
         self._value = new_value
         return self._value
 
-    #def add_child(self, v)
+    def get_due_date(self) -> date|None:
+        return self._due_date
+
+    def set_due_date(self, new_date: date) -> date|None:
+        self._due_date = new_date
+        return self._due_date
+
+    def get_due_time(self) -> time|None:
+        return self._due_time
+
+    def set_due_time(self, new_time: time) -> time|None:
+        self._due_time = new_time
+        return self._due_time
+
+    #def add_child(self, child: Node) -> 
--- test/test_node.py
+++ test/test_node.py
@@ -1,5 +1,6 @@
 import unittest
 from src.node import Node
+from datetime import datetime, timedelta, date, time
 
 class NodeTests(unittest.TestCase):
 
@@ -8,6 +9,29 @@ class NodeTests(unittest.TestCase):
         node_c: Node = Node("Child Task")
         self.assertEqual("Parent Task", node_p.get_value())
         self.assertEqual("Child Task", node_c.get_value())
+        return
 
+    def test_node_due_date(self):
+        tmr: date = date.today() + timedelta(days=1)
+        node_p: Node = Node("Parent Task", due_date=tmr)
+        self.assertEqual(tmr, node_p.get_due_date())
+
+        day_after: date = tmr + timedelta(days=1)
+        _ = node_p.set_due_date(day_after)
+        self.assertEqual(day_after, node_p.get_due_date())
+        return
+
+    def test_node_due_time(self):
+        tmr: date = date.today() + timedelta(days=1)
+        afternoon: time = time(hour=14, minute=30)
+        node_p: Node = Node("Parent Task", due_date=tmr, due_time=afternoon)
+        self.assertEqual(afternoon, node_p.get_due_time())
+
+        evening: time = time(hour=19, minute=30)
+        _ = node_p.set_due_time(evening)
+        self.assertEqual(evening, node_p.get_due_time())
+        return
+        
+        
 if __name__ == "__main__":
     _ = unittest.main()
